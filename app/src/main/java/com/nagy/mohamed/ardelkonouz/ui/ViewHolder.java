788eafd6f6429e35e8bba5056e5099736b138395
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-06-19 01:59:07 +0300

    instructor course screen implement

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ViewHolder.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ViewHolder.java
@@ -514,4 +514,37 @@ public class ViewHolder {
         }
     }
 
+
+    public static class InstructorCourseConnectorViewHolder{
+
+        public final Button SUBMIT_BUTTON;
+        public final Button REST_BUTTON;
+        public final ListView COURSES_LIST_VIEW;
+
+        public InstructorCourseConnectorViewHolder(View instructorCourseView){
+
+            SUBMIT_BUTTON = (Button) instructorCourseView.findViewById(R.id.instructor_course_submit_button);
+            REST_BUTTON = (Button) instructorCourseView.findViewById(R.id.instructor_course_rest_button);
+            COURSES_LIST_VIEW = (ListView) instructorCourseView.findViewById(R.id.instructor_course_list_view);
+        }
+
+        public static class CoursesViewHolder{
+
+            public final TextView COURSE_NAME_TEXT_VIEW;
+            public final TextView COURSE_SALARY_PER_CHILD_TEXT_VIEW;
+            public final TextView COURSE_HOURS_TEXT_VIEW;
+            public final TextView COURSE_DURATION_TEXT_VIEW;
+            public final ImageView COURSE_SELECT_IMAGE_VIEW;
+
+            public CoursesViewHolder(View coursesView){
+
+                COURSE_NAME_TEXT_VIEW = (TextView) coursesView.findViewById(R.id.instructor_course_list_name_text_view);
+                COURSE_SALARY_PER_CHILD_TEXT_VIEW = (TextView) coursesView.findViewById(R.id.instructor_course_list_salary_per_child_text_view);
+                COURSE_HOURS_TEXT_VIEW = (TextView) coursesView.findViewById(R.id.instructor_course_list_hours_text_view);
+                COURSE_DURATION_TEXT_VIEW = (TextView) coursesView.findViewById(R.id.instructor_course_list_duration_text_view);
+                COURSE_SELECT_IMAGE_VIEW = (ImageView) coursesView.findViewById(R.id.instructor_course_list_select__image_view);
+            }
+        }
+    }
+
 }
