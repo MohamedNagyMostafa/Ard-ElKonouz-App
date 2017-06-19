f35f08542737fba198bbbae2672000a68461fa0d
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-06-19 14:57:41 +0300

    employee list dynamic is implemented

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ListScreens/EmployeeActivityFragment.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ListScreens/EmployeeActivityFragment.java
@@ -1,24 +1,127 @@
 package com.nagy.mohamed.ardelkonouz.ui.ListScreens;
 
-import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.database.Cursor;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nagy.mohamed.ardelkonouz.R;
+import com.nagy.mohamed.ardelkonouz.helper.Constants;
+import com.nagy.mohamed.ardelkonouz.offlineDatabase.DatabaseController;
+import com.nagy.mohamed.ardelkonouz.ui.InputScreens.EmployeeInput;
+import com.nagy.mohamed.ardelkonouz.ui.ProfileScreens.EmployeeProfileActivity;
+import com.nagy.mohamed.ardelkonouz.ui.ViewHolder;
+import com.nagy.mohamed.ardelkonouz.ui.adapter.CursorAdapterList;
+import com.nagy.mohamed.ardelkonouz.ui.adapter.DatabaseCursorAdapter;
 
 /**
  * A placeholder fragment containing a simple view.
  */
-public class EmployeeActivityFragment extends Fragment {
+public class EmployeeActivityFragment extends Fragment
+        implements CursorAdapterList, LoaderManager.LoaderCallbacks<Cursor>{
 
-    public EmployeeActivityFragment() {
-    }
+    private DatabaseCursorAdapter databaseCursorAdapter;
+    private ViewHolder.EmployeeListScreenViewHolder employeeListScreenViewHolder;
+
+    private View.OnClickListener addNewEmployeeListener =
+            new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    Intent employeeInputScreen = new Intent(getContext(), EmployeeInput.class);
+                    startActivity(employeeInputScreen);
+                }
+            };
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_employee, container, false);
+        View rootView = inflater.inflate(R.layout.fragment_employee, container, false);
+        employeeListScreenViewHolder = new ViewHolder.EmployeeListScreenViewHolder(rootView);
+
+        databaseCursorAdapter = new DatabaseCursorAdapter(getContext(), null, this);
+
+        employeeListScreenViewHolder.ADD_NEW_EMPLOYEE_BUTTON.setOnClickListener(addNewEmployeeListener);
+        employeeListScreenViewHolder.EMPLOYEE_LIST_VIEW.setAdapter(databaseCursorAdapter);
+
+        getLoaderManager().initLoader(Constants.LOADER_EMPLOYEE_LIST, null, this);
+
+        return rootView;
+    }
+
+    @Override
+    public View newListView(ViewGroup viewGroup, Cursor cursor) {
+        return LayoutInflater.from(getContext())
+                .inflate(R.layout.employee_list_recycle_view, viewGroup,false);
+    }
+
+    @Override
+    public void bindListView(final View view, Cursor cursor) {
+        ViewHolder.EmployeeListScreenViewHolder.EmployeeListRecycleViewHolder
+                employeeListRecycleViewHolder = new ViewHolder.EmployeeListScreenViewHolder
+                .EmployeeListRecycleViewHolder(view);
+
+        employeeListRecycleViewHolder.EMPLOYEE_NAME_TEXT_VIEW.setText(
+                cursor.getString(DatabaseController.ProjectionDatabase.EMPLOYEE_LIST_NAME)
+        );
+
+        employeeListRecycleViewHolder.EMPLOYEE_MOBILE_TEXT_VIEW.setText(
+                cursor.getString(DatabaseController.ProjectionDatabase.EMPLOYEE_LIST_MOBILE)
+        );
+
+        final int employeeId = cursor.getInt(DatabaseController.ProjectionDatabase.EMPLOYEE_LIST_ID);
+
+        employeeListRecycleViewHolder.EMPLOYEE_DELETE_IMAGE_VIEW.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view1) {
+                        // Delete Employee from employee table.
+                        getActivity().getContentResolver().delete(
+                                DatabaseController.UriDatabase.getEmployeeTableWithIdUri(employeeId),
+                                null,
+                                null
+                        );
+
+                        view.setVisibility(View.GONE);
+                        databaseCursorAdapter.notifyDataSetChanged();
+                    }
+                }
+        );
+
+        view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent employeeProfile = new Intent(getContext(), EmployeeProfileActivity.class);
+                employeeProfile.putExtra(Constants.EMPLOYEE_ID_EXTRA, employeeId);
+                startActivity(employeeProfile);
+            }
+        });
+    }
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int id, Bundle args) {
+        return new CursorLoader(
+                getContext(),
+                DatabaseController.UriDatabase.EMPLOYEE_TABLE_URI,
+                DatabaseController.ProjectionDatabase.EMPLOYEE_LIST_PROJECTION,
+                null,
+                null,
+                null
+        );
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Cursor> loader, Cursor data) {
+        databaseCursorAdapter.swapCursor(data);
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Cursor> loader) {
+        databaseCursorAdapter.swapCursor(null);
     }
 }
