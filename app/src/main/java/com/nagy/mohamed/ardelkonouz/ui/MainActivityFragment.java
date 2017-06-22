b76eb0457f72e45a2331d7c5fb61a35fd0a74c76
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-06-22 14:52:49 +0300

    Time Picker Date Picker implement

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/MainActivityFragment.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/MainActivityFragment.java
@@ -4,34 +4,23 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.nagy.mohamed.ardelkonouz.R;
-import com.nagy.mohamed.ardelkonouz.calenderFeature.CurrentDateWithTime;
-import com.nagy.mohamed.ardelkonouz.calenderFeature.DatePickerFragment;
-import com.nagy.mohamed.ardelkonouz.calenderFeature.TimePickerFragment;
 import com.nagy.mohamed.ardelkonouz.ui.ListScreens.ChildActivity;
 import com.nagy.mohamed.ardelkonouz.ui.ListScreens.CourseActivity;
 import com.nagy.mohamed.ardelkonouz.ui.ListScreens.EmployeeActivity;
 import com.nagy.mohamed.ardelkonouz.ui.ListScreens.InstructorActivity;
 import com.nagy.mohamed.ardelkonouz.ui.SalaryScreens.SalaryActivity;
 
-import java.text.DateFormat;
-import java.util.Calendar;
-import java.util.Date;
-
 /**
  * A placeholder fragment containing a simple view.
  */
-public class MainActivityFragment extends Fragment
-        implements CurrentDateWithTime{
-
+public class MainActivityFragment extends Fragment{
 
     private ViewHolder.MainScreenViewHolder mainScreenViewHolder;
-    private Calendar calendar;
 
     private View.OnClickListener courseListClickListener = new View.OnClickListener() {
         @Override
@@ -84,7 +73,6 @@ public class MainActivityFragment extends Fragment
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        databaseTesting();
-        calendar = Calendar.getInstance();
 
     }
 
@@ -101,40 +89,40 @@ public class MainActivityFragment extends Fragment
         mainScreenViewHolder.EMPLOYEE_LIST_LAYOUT.setOnClickListener(employeeListClickListener);
         mainScreenViewHolder.INSTRUCTOR_LIST_LAYOUT.setOnClickListener(instructorListClickListener);
         mainScreenViewHolder.SALARY_LIST_LAYOUT.setOnClickListener(salaryListClickListener);
-
-        DatePickerFragment datePickerFragment = new DatePickerFragment();
-        TimePickerFragment timePickerFragment = new TimePickerFragment();
-        datePickerFragment.setCurrentDateWithTime(this);
-        timePickerFragment.setCurrentDateWithTime(this);
-        datePickerFragment.show(getFragmentManager(),"d");
-        timePickerFragment.show(getFragmentManager(),"d");
+//
+//        DatePickerFragment datePickerFragment = new DatePickerFragment();
+//        TimePickerFragment timePickerFragment = new TimePickerFragment();
+//        datePickerFragment.setCurrentDateWithTime(this);
+//        timePickerFragment.setCurrentDateWithTime(this);
+//        datePickerFragment.show(getFragmentManager(),"d");
+//        timePickerFragment.show(getFragmentManager(),"d");
 
 
         return rootView;
     }
-
-    @Override
-    public void onTimeSet(int hour, int mint) {
-        calendar.set(Calendar.HOUR_OF_DAY, hour);
-        calendar.set(Calendar.MINUTE, mint);
-    }
-
-    @Override
-    public void onDateSet(int year, int month, int day) {
-        calendar.set(Calendar.YEAR, year);
-        calendar.set(Calendar.DAY_OF_WEEK, day);
-        calendar.set(Calendar.MONTH, month);
-        DateFormat dateFormat = DateFormat.getDateInstance();
-        Date date = new Date();
-        Date date1 = new Date();
-        date.setTime(calendar.getTimeInMillis());
-        Log.e(DateFormat.getDateTimeInstance().format(date), String.valueOf(calendar.getTimeInMillis()));
-        Calendar calendar1 = Calendar.getInstance();
-        date1.setTime(calendar1.getTimeInMillis());
-        Log.e(DateFormat.getDateTimeInstance().format(date1), String.valueOf(calendar1.getTimeInMillis()));
-
-        Log.e("time now diff",String .valueOf(calendar.getTimeInMillis() - calendar1.getTimeInMillis()));
-
-    }
+//
+//    @Override
+//    public void onTimeSet(int hour, int mint) {
+//        calendar.set(Calendar.HOUR_OF_DAY, hour);
+//        calendar.set(Calendar.MINUTE, mint);
+//    }
+//
+//    @Override
+//    public void onDateSet(int year, int month, int day) {
+//        calendar.set(Calendar.YEAR, year);
+//        calendar.set(Calendar.DAY_OF_WEEK, day);
+//        calendar.set(Calendar.MONTH, month);
+//        DateFormat dateFormat = DateFormat.getDateInstance();
+//        Date date = new Date();
+//        Date date1 = new Date();
+//        date.setTime(calendar.getTimeInMillis());
+//        Log.e(DateFormat.getDateTimeInstance().format(date), String.valueOf(calendar.getTimeInMillis()));
+//        Calendar calendar1 = Calendar.getInstance();
+//        date1.setTime(calendar1.getTimeInMillis());
+//        Log.e(DateFormat.getDateTimeInstance().format(date1), String.valueOf(calendar1.getTimeInMillis()));
+//
+//        Log.e("time now diff",String .valueOf(calendar.getTimeInMillis() - calendar1.getTimeInMillis()));
+//
+//    }
 
 }
