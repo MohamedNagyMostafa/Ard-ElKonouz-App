57124d40a125b767bae0404e5e028833e240b060
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-06-22 12:51:03 +0300

    instructor connector course implement is completed

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ProfileScreens/ConnectorsScreen/InstructorCourseConnectorActivityFragment.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ProfileScreens/ConnectorsScreen/InstructorCourseConnectorActivityFragment.java
@@ -95,7 +95,6 @@ public class InstructorCourseConnectorActivityFragment extends Fragment
                                 contentValues
                         );
 
-
                         openInstructorProfile();
                     }
                 }
