efaf4bc8229583ba2b9b6a3ac8cfbd25f73cfb52
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-07-31 01:53:29 +0300

    section profile back button implement

---
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/MainActivity.java
@@ -0,0 +1,26 @@
+package com.nagy.mohamed.ardelkonouz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.WindowManager;
+
+import com.nagy.mohamed.ardelkonouz.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().setDisplayShowTitleEnabled(false);
+
+
+    }
+
+}
