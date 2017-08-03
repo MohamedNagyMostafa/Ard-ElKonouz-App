e28edb6f055928b15b9060fa1b7df222e54348d8
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-08-03 14:27:48 +0300

    salary data updated

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/offlineDatabase/DatabaseController.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/offlineDatabase/DatabaseController.java
@@ -14,6 +14,30 @@ public class DatabaseController {
     public static class ProjectionDatabase {
 
 
+
+        // Salary
+        public static final String[] SALARY_PROJECTION = {
+                DbContent.CourseTable.COURSE_SALARY_PER_CHILD,
+                DbContent.CourseTable.COURSE_COST_COLUMN
+        };
+
+        public static final int SALARY_COURSE_PERCENT_PER_CHILD = 0;
+        public static final int SALARY_COURSE_COST = 1;
+
+        public static final String[] SECTION_INSTRUCTOR_SALARY_PROJECTION={
+                DbContent.SectionInstructorTable.TABLE_NAME + "."+ DbContent.SectionInstructorTable._ID,
+                DbContent.SectionInstructorTable.PAID_COLUMN,
+                DbContent.SectionTable.SECTION_NAME_COLUMN,
+                DbContent.InstructorTable.INSTRUCTOR_NAME_COLUMN,
+                DbContent.SectionInstructorTable.INSTRUCTOR_ID_COLUMN,
+                DbContent.SectionInstructorTable.SECTION_ID_COLUMN
+        };
+
+        public static final int SECTION_INSTRUCTOR_SALARY_PAID = 1;
+        public static final int SECTION_INSTRUCTOR_SALARY_SECTION_NAME = 2;
+        public static final int SECTION_INSTRUCTOR_SALARY_INSTRUCTOR_NAME = 3;
+        public static final int SECTION_INSTRUCTOR_SALARY_INSTRUCTOR_ID = 4;
+        public static final int SECTION_INSTRUCTOR_SALARY_SECTION_ID = 5;
         // Child list
         public static final String[] CHILD_LIST_SECTION_PROJECTION = {
                 DbContent.SectionTable.TABLE_NAME + "." + DbContent.SectionTable._ID,
@@ -197,15 +221,15 @@ public class DatabaseController {
         public static final int INSTRUCTOR_LIST_ID = 0;
         public static final int INSTRUCTOR_LIST_NAME = 1;
 
-//        public static final String[] SECTION_INSTRUCTOR_TABLE = {
-//                DbContent.SectionInstructorTable.SECTION_ID_COLUMN,
-//                DbContent.SectionInstructorTable.INSTRUCTOR_ID_COLUMN,
-//                DbContent.SectionInstructorTable.PAID_COLUMN
-//        };
-//
-//        public static final int SECTION_INSTRUCTOR_COURSE_ID = 0;
-//        public static final int SECTION_INSTRUCTOR_INSTRUCTOR_ID = 1;
-//        public static final int SECTION_INSTRUCTOR_PAID =2;
+        public static final String[] SECTION_INSTRUCTOR_TABLE = {
+                DbContent.SectionInstructorTable.SECTION_ID_COLUMN,
+                DbContent.SectionInstructorTable.INSTRUCTOR_ID_COLUMN,
+                DbContent.SectionInstructorTable.PAID_COLUMN
+        };
+
+        public static final int SECTION_INSTRUCTOR_SECTION_ID = 0;
+        public static final int SECTION_INSTRUCTOR_INSTRUCTOR_ID = 1;
+        public static final int SECTION_INSTRUCTOR_PAID =2;
 
         public static final String[] SECTION_INSTRUCTOR_CONNECTOR_JOIN_TABLE = {
                 DbContent.SectionTable.TABLE_NAME + "." + DbContent.SectionTable._ID,
