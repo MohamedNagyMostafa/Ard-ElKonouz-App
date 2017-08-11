52de0ba24b1b9b451dc2163d327e16ff963cfe54
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-08-11 21:16:09 +0300

    course input screen back button implement

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/InputScreens/CourseInputActivity.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/InputScreens/CourseInputActivity.java
@@ -1,13 +1,19 @@
 package com.nagy.mohamed.ardelkonouz.ui.InputScreens;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 import android.view.WindowManager;
 
 import com.nagy.mohamed.ardelkonouz.R;
+import com.nagy.mohamed.ardelkonouz.helper.Constants;
+import com.nagy.mohamed.ardelkonouz.ui.ListScreens.CourseActivity;
+import com.nagy.mohamed.ardelkonouz.ui.ProfileScreens.CourseProfileActivity;
 
 public class CourseInputActivity extends AppCompatActivity {
 
@@ -29,4 +35,31 @@ public class CourseInputActivity extends AppCompatActivity {
         getSupportActionBar().setDisplayShowTitleEnabled(false);
     }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        switch (item.getItemId()){
+            case android.R.id.home:
+                openScreen();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    private void openScreen(){
+        final String INPUT_TYPE = getIntent().getExtras().getString(Constants.INPUT_TYPE_EXTRA);
+
+        if(INPUT_TYPE.equals(Constants.INPUT_ADD_EXTRA)){
+            Intent courseListScreen = new Intent(this, CourseActivity.class);
+            startActivity(courseListScreen);
+            NavUtils.navigateUpTo(this, courseListScreen);
+        }else{
+            final Long COURSE_ID = getIntent().getExtras().getLong(Constants.COURSE_ID_EXTRA);
+
+            Intent courseProfileScreen = new Intent(this, CourseProfileActivity.class);
+            courseProfileScreen.putExtra(Constants.COURSE_ID_EXTRA, COURSE_ID);
+            startActivity(courseProfileScreen);
+            NavUtils.navigateUpTo(this, courseProfileScreen);
+        }
+    }
 }
