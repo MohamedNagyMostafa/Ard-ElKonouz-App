61715ebb40d7372ace894bb651855a2ae47dbe4c
mohamed nagy mostafa <mohamednagy2015@outlook.com>
2017-08-13 23:10:28 +0300

    add main button to all profiles

--- app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ProfileScreens/InstructorProfileActivity.java
+++ app/src/main/java/com/nagy/mohamed/ardelkonouz/ui/ProfileScreens/InstructorProfileActivity.java
@@ -1,13 +1,16 @@
 package com.nagy.mohamed.ardelkonouz.ui.ProfileScreens;
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
 import android.view.WindowManager;
 
 import com.nagy.mohamed.ardelkonouz.R;
+import com.nagy.mohamed.ardelkonouz.ui.mainScreen.MainActivity;
 
 public class InstructorProfileActivity extends AppCompatActivity {
 
@@ -27,6 +30,21 @@ public class InstructorProfileActivity extends AppCompatActivity {
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
+        findViewById(R.id.instructor_pf_main_icon_image_view).setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        openMainScreen();
+                    }
+                }
+        );
+
+    }
+
+    private void openMainScreen(){
+        Intent mainScreen = new Intent(this, MainActivity.class);
+        startActivity(mainScreen);
+        finish();
     }
 
 }
